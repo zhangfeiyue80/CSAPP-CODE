81ced12f32b38344cf15521c4b033c7382fc14c6
zhangfeiyue80 <2247058872@qq.com>
2019-06-02 11:55:12 +0800

    20190602

--- CSAPP/tools/main.cpp
+++ CSAPP/tools/main.cpp
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
+#ifdef DEBUG
+#define DBGprint(...) printf(__VA_ARGS__)
+#else
+#define DBGprint(...)
+#endif
+
 // long int number(hex to decimalization)
 int show_value()
 {
@@ -10,11 +16,18 @@ int show_value()
 	printf("please input a little_endian hex number:");
 	scanf("%s",str);
 	len=strlen(str);
-	for(i=0;i<len-2;i=i+2)
+//debug
+	DBGprint("len is %d\n",len);
+
+	for(i=0;i<len;i=i+2)
 	{
-		vstr[i]=str[len-3-i];
-		vstr[i+1]=str[len-i-2];
+		vstr[i]=str[len-2-i];
+		vstr[i+1]=str[len-i-1];
 	}
+	vstr[i]='\0';
+//debug
+	DBGprint("vstr is %s\n",vstr);
+
 	sscanf(vstr,"%x",&number);
 	printf("the value of inputed little_endian hex number is: %ld",(long int)number);
 	return 0;
